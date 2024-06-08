661c2ba73a1073f675c97368359b3dd2518ffebd
kumarmanojgit <krmanoj72099@gmail.com>
2024-06-08 13:42:11 +0530

    connection with database

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,27 @@
 import express from "express";
 // const express=require('express')
-
 import cors from "cors"
 import dotenv from "dotenv";
+import { dbConnection } from "./database/dbConnection.js";
+
+
 const app=express()
 dotenv.config({path:"./config/config.env"});
+
+
+// frontend to backend connection using middleware
+app.use(cors({
+  origin:[process.env.FRONTEND_URL],
+  method:["POST"],
+  credentials:true,
+}));
+
+
+//store the object in json format.
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+dbConnection();
 export default app;
 
 
---
+++ Backend/database/dbConnection.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+export const dbConnection=()=>
+  {
+    mongoose.connect(process.env.MONGO_URI,{dbName:"restaurent"})
+    .then(()=>{console.log("connected to database successfully");})
+    .catch((err)=>{
+      console.log(`some error${err}`);
+    });
+  };
